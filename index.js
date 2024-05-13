1c7aa6619c36401d84a99d7f9cdbddeb292faefd
asfas12e <js1minnimod@hotmail.com>
2024-05-13 12:34:02 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function minMeetingRooms(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-  const minHeap = new MinHeap();
-  for (const interval of intervals) {
-    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
-      minHeap.pop();
-    }
-    minHeap.push(interval[1]);
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) {
+    result ^= num;
   }
-  return minHeap.size();
+  return result;
 }
